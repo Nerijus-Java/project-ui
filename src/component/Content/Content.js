bc22c317b27bfdd6c76774854034cf9b172f68d9
Nerijus Viliusis <pcneriyus3@gmail.com>
2021-06-01 15:49:10 +0300

    basic like animation added

--- src/component/Content/Content.js
+++ src/component/Content/Content.js
@@ -1,22 +1,34 @@
-import {Route, Switch} from "react-router-dom"
+import {Route, Switch, useLocation} from "react-router-dom"
 import LandingPage from "../Pages/LandingPage/LandingPage";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
-import {Container, Paper} from "@material-ui/core";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+import "../../styles.css"
 
 
-export default () => (
-    <>
-        <Switch>
-            <Route exact path="/">
-                <LandingPage/>
-            </Route>
-            <Route path="/login">
-                <Login/>
-            </Route>
-            <Route path="/register">
-                <Register/>
-            </Route>
-        </Switch>
-    </>
-)
+function Content() {
+
+    const location = useLocation()
+
+    return(
+        <>
+            <TransitionGroup>
+                <CSSTransition timeout={250} classNames='fade' key={location.key}>
+                    <Switch location={location}>
+                        <Route exact path="/">
+                            <LandingPage/>
+                        </Route>
+                        <Route path="/login">
+                            <Login/>
+                        </Route>
+                        <Route path="/register">
+                            <Register/>
+                        </Route>
+                    </Switch>
+                </CSSTransition>
+            </TransitionGroup>
+        </>
+    )
+}
+
+export default Content
